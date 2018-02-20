a058d0ad7120f24c86106c1ff69107e363ac6bb2
jacobbrunson <jacob@brunson.me>
2018-02-20 17:28:28 -0600

    added basic search API

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const items = require('./items')
+
+
+app.get('/search', (req, res) => {
+	results = []
+
+	for (index in items) {
+		item = items[index]
+		if (item.name.includes(req.query.name)) {
+			results.push(item)
+		}
+	}
+
+	res.send({items: results})
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'))
---
+++ items.js
@@ -0,0 +1,5 @@
+module.exports = [
+	{name: 'bread', price: 2.99},
+	{name: 'peanut butter', price: 1.99},
+	{name: 'grape jelly', price: 2.49},
+]
