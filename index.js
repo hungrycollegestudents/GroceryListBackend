e3abd6bd8063ac00ed24a1bf704e86a1cfaebb60
jacobbrunson <jacob@brunson.me>
2018-02-27 16:23:04 -0600

    changed port to environment var

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ app.get('/search', (req, res) => {
 	res.send({items: results})
 })
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'))
